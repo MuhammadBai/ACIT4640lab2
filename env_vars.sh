b0577b26bf81ae286ab2efa10a3be04d0f8fbb48
vboxuser <vboxuser@Debian.myguest.virtualbox.org>
2026-01-21 19:29:42 -0800

    Completed Part 3 nginx install and document scripts

--- env_vars.sh
+++ env_vars.sh
@@ -1,3 +1,6 @@
 USERNAME="admin"
-IP_ADDRESS="18.220.27.165"
-SSH_KEY_PATH="/home/SarahLinux/part1-scripts/wkone"
+IP_ADDRESS="54.185.138.129"
+# Sarah's environment
+# SSH_KEY_PATH="/home/SarahLinux/part1-scripts/wkone"
+# Muhammad's environment
+SSH_KEY_PATH="/home/vboxuser/.ssh/wkone"
